099745f924b2f4ccd47ec24c2e9d8f0052307358
sayalipawade <sayalipawade05@gmail.com>
2020-05-28 16:52:20 +0530

    Added:Code for dictionary

---
+++ dict.py
@@ -0,0 +1,9 @@
+dictionary_tk = {
+  "name": "Sayali",
+  "nickname": "Sonu",
+  "nationality": "Indian",
+  "age": 23
+}
+
+for attribute, value in dictionary_tk.items():
+    print("My %s is %s" %(attribute, value))
